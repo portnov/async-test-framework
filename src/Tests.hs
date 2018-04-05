342a0a783d325b1c865f3ba0b347a0d97cb94400
Ilya Portnov <portnov@bk.ru>
2018-04-05 22:17:39 +0500

    Logging.

--- src/Tests.hs
+++ src/Tests.hs
@@ -1,9 +1,11 @@
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
 
 module Tests where
 
 import Control.Monad
+import Control.Monad.Trans
 import Control.Concurrent
 import Control.Distributed.Process hiding (bracket, finally)
 import Control.Distributed.Process.Node
@@ -17,6 +19,10 @@ import Network.Transport.TCP (createTransport, defaultTCPParameters)
 import GHC.Generics
 import System.Random
 import Text.Printf
+import System.Log.Heavy
+-- import System.Log.Heavy.Shortcuts
+import System.Log.Heavy.TH
+import Data.Text.Format.Heavy
 
 import Types
 import Connection
@@ -36,31 +42,31 @@ instance IsMessage MyMessage where
   isAdministrative _ = False
   getMatchKey m = fromString (show $ mKey m)
 
-reader :: ExtPort -> Process ()
+reader :: ExtPort -> AProcess ()
 reader port = do
-    liftIO $ putStrLn $ "hello from reader: " ++ show port
+    $info "hello from reader: {}" (Single $ show port)
     loop `finally` closeServerConnection
   where
     loop = forever $ do
             msg <- liftIO $ readMessage port SimpleFrame
-            nsend "worker" (msg :: MyMessage)
+            lift $ nsend "worker" (msg :: MyMessage)
 
     closeServerConnection =
       case port of
         ServerPort _ conn _ -> liftIO $ close conn
         _ -> return ()
 
-writer :: ExtPort -> Process ()
+writer :: ExtPort -> AProcess ()
 writer port = do
     let portNumber = getPortNumber port
-    self <- getSelfPid
-    register ("writer:"++show portNumber) self
+    self <- lift getSelfPid
+    lift $ register ("writer:"++show portNumber) self
     loop `finally` closeServerConnection
   where
     loop =
         forever $ do
-          liftIO $ putStrLn $ "hello from writer: " ++ show port
-          msg <- expect
+          $info "hello from writer: {}" (Single $ show port)
+          msg <- lift expect
           liftIO $ writeMessage port SimpleFrame (msg :: MyMessage)
 
     closeServerConnection =
@@ -68,63 +74,68 @@ writer port = do
         ServerPort _ conn _ -> liftIO $ close conn
         _ -> return ()
 
-clientWorker :: [PortNumber] -> Int -> Process ()
+clientWorker :: [PortNumber] -> Int -> AProcess ()
 clientWorker ports base = do
-  self <- getSelfPid
-  register "worker" self
-  liftIO $ putStrLn "hello from client worker"
+  self <- lift getSelfPid
+  lift $ register "worker" self
+  $info "hello from client worker" ()
   forM_ [1 .. 10] $ \id -> do
     let key = (base + (2*id)) :: Int
     let msg = MyMessage False key "request"
-    liftIO $ putStrLn $ "client request: " ++ show msg
+    $info "client request: {}" (Single $ show msg)
     let n = length ports
     idx <- liftIO $ randomRIO (0,n-1)
     let portNumber = (ports !! idx)
-    nsend ("writer:"++show portNumber) msg
-    liftIO $ putStrLn $ "client sent request: " ++ show msg
-    msg' <- expect
-    liftIO $ printf "client worker %s: response received: %s\n" (show base) (show (msg' :: MyMessage))
+    lift $ nsend ("writer:"++show portNumber) msg
+    $info "client sent request: {}" (Single $ show msg)
+    msg' <- lift expect
+    $info "client worker {}: response received: {}" (show base, show (msg' :: MyMessage))
 
-serverWorker :: [PortNumber] -> Process ()
+serverWorker :: [PortNumber] -> AProcess ()
 serverWorker ports = do
-  self <- getSelfPid
-  register "worker" self
-  liftIO $ putStrLn "hello from server worker"
+  self <- lift getSelfPid
+  lift $ register "worker" self
+  $info "hello from server worker" ()
   forever $ do
-    msg <- expect
-    liftIO $ printf "server worker: request received: %s\n" (show (msg :: MyMessage))
+    msg <- lift expect
+    $info "server worker: request received: {}" (Single $ show (msg :: MyMessage))
     let msg' = msg {mIsResponse = True, mKey = mKey msg + 1, mPayload = "response"}
     let n = length ports
     idx <- liftIO $ randomRIO (0,n-1)
     let portNumber = (ports !! idx)
-    nsend ("writer:"++show portNumber) msg'
-    liftIO $ putStrLn "server worker: response sent"
+    lift $ nsend ("writer:"++show portNumber) msg'
+    $info "server worker: response sent" ()
 
 localhost = "127.0.0.1"
 
 mkExternalService port = (localhost, port)
+
+logSettings = defaultSyslogSettings {ssFormat = "[{level}] {source} {process} {thread}: {message}"}
 
 runClient minPort maxPort = do
   Right transport <- createTransport localhost "10501" mkExternalService defaultTCPParameters
   node <- newLocalNode transport initRemoteTable
 
   runProcess node $ do
-      forM_ [minPort .. maxPort] $ \portNumber -> do
-          spawnLocal $ clientConnection localhost portNumber $ \extPort -> do
-            w <- spawnLocal (writer extPort)
-            link w
-            liftIO $ putStrLn "client spawned writer"
-            r <- spawnLocal (reader extPort)
-            link r
-            liftIO $ putStrLn "client spawned reader"
-            return ()
+    withLoggingT (LoggingSettings logSettings) $ do
+      let frame = LogContextFrame [("process", Variable ("client" :: String))] noChange
+      withLogContext frame $ do
+        forM_ [minPort .. maxPort] $ \portNumber -> do
+            spawnAProcess $ clientConnection localhost portNumber $ \extPort -> do
+              w <- spawnAProcess (writer extPort)
+              lift $ link w
+              $info "client spawned writer" ()
+              r <- spawnAProcess (reader extPort)
+              lift $ link r
+              $info "client spawned reader" ()
+              return ()
 
-      liftIO $ threadDelay $ 100*1000
+        liftIO $ threadDelay $ 100*1000
 
-      liftIO $ putStrLn "hello"
-      spawnLocal $
-          clientWorker [minPort .. maxPort] 100 
-      return ()
+        $info "hello" ()
+        spawnAProcess $
+            clientWorker [minPort .. maxPort] 100 
+        return ()
 
   putStrLn "hit enter..."
   getLine
@@ -136,19 +147,22 @@ runServer minPort maxPort = do
 
   putStrLn "hello"
   runProcess node $ do
-      spawnLocal $ serverWorker [minPort .. maxPort]
+    withLoggingT (LoggingSettings logSettings) $ do
+      let frame = LogContextFrame [("process", Variable ("server" :: String))] noChange
+      withLogContext frame $ do
+        spawnAProcess $ serverWorker [minPort .. maxPort]
 
-      liftIO $ threadDelay $ 100*1000
-        
-      forM_ [minPort .. maxPort] $ \portNumber -> do
-          spawnLocal $ serverConnection localhost portNumber $ \extPort -> do
-            r <- spawnLocal (reader extPort)
-            link r
-            liftIO $ putStrLn $ "server spawned reader: " ++ show extPort
-            w <- spawnLocal (writer extPort)
-            link w
-            liftIO $ putStrLn $ "server spawned writer: " ++ show extPort
-            return ()
+        liftIO $ threadDelay $ 100*1000
+          
+        forM_ [minPort .. maxPort] $ \portNumber -> do
+            spawnAProcess $ serverConnection localhost portNumber $ \extPort -> do
+              r <- spawnAProcess (reader extPort)
+              lift $ link r
+              $info "server spawned reader: {}" (Single $ show extPort)
+              w <- spawnAProcess (writer extPort)
+              lift $ link w
+              $info "server spawned writer: {}" (Single $ show extPort)
+              return ()
 
   putStrLn "hit enter..."
   getLine
--- src/Types.hs
+++ src/Types.hs
@@ -1,12 +1,19 @@
+{-# LANGUAGE OverloadedStrings #-}
 
 module Types where
 
+import Control.Monad.Reader
 import Control.Concurrent
 import Control.Concurrent.STM
+import Control.Distributed.Process hiding (bracket, mask, catch)
+import Control.Monad.Catch 
 import qualified Data.Map as M
+import qualified Data.Text as T
 import Data.Binary
 import qualified Data.ByteString.Lazy as L
 import Network.Socket
+import System.Log.Heavy
+import Data.Text.Format.Heavy
 
 data ExtPort =
   ClientPort PortNumber Socket
@@ -36,4 +43,30 @@ data ConnectionPool = ConnectionPool {
     cpSettings :: PoolSettings,
     cpPorts :: M.Map PortNumber ExtPort
   }
+
+instance MonadThrow m => MonadThrow (LoggingT m) where
+  throwM e = LoggingT $ lift $ throwM e
+
+instance MonadCatch m => MonadCatch (LoggingT m) where
+  catch (LoggingT (ReaderT m)) c = LoggingT $ ReaderT $ \r -> m r `catch` \e -> runLoggingT (c e) r
+
+instance MonadMask m => MonadMask (LoggingT m) where
+  mask a = LoggingT $ ReaderT $ \lts -> mask $ \u -> runLoggingT (a $ q u) lts
+    where q :: (m a -> m a) -> LoggingT m a -> LoggingT m a
+          q u (LoggingT (ReaderT b)) = LoggingT (ReaderT (u . b))
+
+  uninterruptibleMask a = LoggingT $ ReaderT $ \lts -> uninterruptibleMask $ \u -> runLoggingT (a $ q u) lts
+    where q :: (m a -> m a) -> LoggingT m a -> LoggingT m a
+          q u (LoggingT (ReaderT b)) = LoggingT (ReaderT (u . b))
+
+type AProcess a = LoggingT Process a
+
+spawnAProcess :: AProcess () -> AProcess ProcessId
+spawnAProcess proc = do
+  lts <- LoggingT ask
+  lift $ spawnLocal $ do
+      self <- getSelfPid
+      let frame = LogContextFrame [("thread", Variable (show self))] noChange
+      let lts' = lts {ltsContext = frame : ltsContext lts}
+      runLoggingT proc lts'
 
